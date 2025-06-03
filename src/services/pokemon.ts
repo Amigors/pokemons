0ced76bd0c8321ff0180d9e916888ef2749c42fa
Lubinets Sergey <lubinets.sn@yndex.ru>
2025-06-03 18:42:39 +0300

    queries

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,18 +1,17 @@
-// Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import type { Pokemon } from './types'
 
-// Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
   endpoints: (builder) => ({
-    getPokemonByName: builder.query<unknown, string>({
-      query: () => `pokemon`,
+    getAllPokemons: builder.query<any, { limit: number; offset?: number }>({
+      query: ({ limit, offset = 0 }) =>
+        `pokemon?limit=${limit}&offset=${offset}`,
+    }),
+    getPokemonByName: builder.query<any, string>({
+      query: (name) => `pokemon/${name}`,
     }),
   }),
 });
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi;
+export const { useGetAllPokemonsQuery, useGetPokemonByNameQuery } = pokemonApi;
